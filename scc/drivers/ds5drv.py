1cd282ff7b76f842ec148243b85e18d67fde2dba
Zach Kanzler <they4kman@gmail.com>
2022-06-20 00:47:26 -0400

    feat(ds5): add support for changing lightbar colour based on controller icon

--- scc/drivers/ds5drv.py
+++ scc/drivers/ds5drv.py
@@ -89,6 +89,17 @@ class DualSenseHIDOutput(ctypes.Structure):
     ]
 
 
+ICON_COLORS = [
+    (0.0, 1.0, 0.0),  # 0
+    (0.0, 0.0, 1.0),  # 1
+    (1.0, 0.0, 0.0),  # 2
+    (1.0, 1.0, 0.0),  # 3
+    (0.0, 1.0, 1.0),  # 4
+    (1.0, 0.4, 0.0),  # 5
+    (1.0, 0.0, 1.0),  # 6
+]
+
+
 class DS5Controller(HIDController):
     # Most of axes are the same
     BUTTON_MAP = (
@@ -117,16 +128,15 @@ class DS5Controller(HIDController):
              )
 
     def __init__(self, device, daemon, handle, config_file, config, test_mode=False):
-        super(DS5Controller, self).__init__(device, daemon, handle, config_file, config, test_mode)
-        self._imsg = []
-        self._output_dirty = False
-        self._output = DualSenseHIDOutput(
+        self._outputs = {}
+        self._feedback_output = DualSenseHIDOutput(
             operating_mode=OperatingMode.DS5_MODE,
             physical_effect_control=PhysicalEffectControl.ENABLE_HAPTICS,
             motor_left=0,
             motor_right=0,
         )
         self._feedback_cancel_task = None
+        super(DS5Controller, self).__init__(device, daemon, handle, config_file, config, test_mode)
 
     def _load_hid_descriptor(self, config, max_size, vid, pid, test_mode):
         # Overrided and hardcoded
@@ -268,37 +278,61 @@ class DS5Controller(HIDController):
 
         if position == HapticPos.LEFT:
             # NOTE: the left motor is heavier, so we must give it less oomph
-            self._output.motor_left = half_amp
+            self._feedback_output.motor_left = half_amp
         elif position == HapticPos.RIGHT:
-            self._output.motor_right = clamped_amp
+            self._feedback_output.motor_right = clamped_amp
         elif position == HapticPos.BOTH:
-            self._output.motor_right = clamped_amp
-            self._output.motor_left = half_amp
+            self._feedback_output.motor_right = clamped_amp
+            self._feedback_output.motor_left = half_amp
 
         duration = float(period) * count / 0x10000
         # The motors don't seem to perform reliably when shut off under 20ms
         duration = max(duration, 0.02)
 
-        self.schedule_output()
+        self.schedule_output('feedback', self._feedback_output)
 
         def clear_feedback(mapper):
-            self._output.motor_right = self._output.motor_left = 0
-            self.schedule_output()
+            self._feedback_output.motor_right = self._feedback_output.motor_left = 0
+            self.schedule_output('feedback', self._feedback_output)
 
         if self._feedback_cancel_task:
             self._feedback_cancel_task.cancel()
         self._feedback_cancel_task = self.mapper.schedule(duration, clear_feedback)
 
-    def schedule_output(self):
-        self._output_dirty = True
+    def apply_config(self, config):
+        icon = config['icon']
+        led_level = config['led_level']
+        self.configure(icon=icon, led_level=led_level)
 
-    def flush(self):
-        super(DS5Controller, self).flush()
+    def configure(self, icon=None, led_level=100):
+        lightbar_color = (0.0, 0.0, 1.0)  # blue by default
+        if icon:
+            basename, ext = icon.rsplit('.', 1)
+            parts = basename.rsplit('-', 1)
+            if parts:
+                raw_idx = parts[-1]
+                try:
+                    icon_idx = int(raw_idx)
+                except ValueError:
+                    pass
+                else:
+                    if icon_idx < len(ICON_COLORS):
+                        lightbar_color = ICON_COLORS[icon_idx]
 
-        if self._output_dirty:
-            data = bytes(bytearray(self._output).ljust(64, b'\x00'))
-            self.handle.interruptWrite(3, data)
-            self._output_dirty = False
+        led_level_norm = float(led_level) / 100
+        lightbar_color_bytes = tuple(
+            int(color_norm * led_level_norm * 255)
+            for color_norm in lightbar_color
+        )
+
+        output = DualSenseHIDOutput(
+            operating_mode=OperatingMode.DS5_MODE,
+            light_effect_control=LightEffectControl.LIGHTBAR_CONTROL_ENABLE,
+            lightbar_red=lightbar_color_bytes[0],
+            lightbar_green=lightbar_color_bytes[1],
+            lightbar_blue=lightbar_color_bytes[2],
+        )
+        self.schedule_output('lightbar', output)
 
     def get_gyro_enabled(self):
         # Cannot be actually turned off, so it's always active
@@ -325,6 +359,17 @@ class DS5Controller(HIDController):
             id = "ds5:%s" % (magic_number,)
             magic_number += 1
         return id
+
+    def schedule_output(self, output_id, output):
+        self._outputs[output_id] = output
+
+    def flush(self):
+        super(DS5Controller, self).flush()
+
+        while self._outputs:
+            output_id, output = self._outputs.popitem()
+            data = bytes(bytearray(output).ljust(64, b'\x00'))
+            self.handle.interruptWrite(3, data)
 
 
 class DS5EvdevController(EvdevController):
